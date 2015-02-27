714de96dfe14c585adeeeb97be2a45ad1946c42d
Simon Verhoek <saverhoek@gmail.com>
2015-02-27 09:53:53 +0100

    fixed identation to width of 4

--- popup.js
+++ popup.js
@@ -1,34 +1,33 @@
 var append = function(text) {
-  data.appendChild(document.createTextNode(text));
+    data.appendChild(document.createTextNode(text));
 }
 
 var download = function(format) {
-  document.getElementById('content').innerText = "preparing file...";
+    document.getElementById('content').innerText = "preparing file...";
 
-  chrome.history.search({
-    'text': '', 
-    // 'maxResults': 100, 
-    'maxResults': 100000, 
-    'startTime': 0
-  }, function(res) {
-    window.res = res;
+    chrome.history.search({
+        'text': '', 
+        // 'maxResults': 100, 
+        'maxResults': 100000, 
+        'startTime': 0
+    }, function(res) {
+        window.res = res;
 
-    var text, filename;
+        var text, filename;
 
-    // put the data in a hidden div so chrome doesn't crash
-    for(var i = 0; i < res.length; i++) {
-      if (format === "urlsonly") {
-        filename = "urlsonly.json";
-        text = JSON.stringify(res[i].url);
-      } else {
-        filename = "alldata.json";
-        text = JSON.stringify(res[i]);
-      }
-      if (i !== res.length - 1) text = text + ',';
-      append(text);
+        // put the data in a hidden div so chrome doesn't crash
+        for(var i = 0; i < res.length; i++) {
+            if (format === "urlsonly") {
+                filename = "urlsonly.json";
+                text = JSON.stringify(res[i].url);
+            } else {
+                filename = "alldata.json";
+                text = JSON.stringify(res[i]);
+            };
+            if (i !== res.length - 1) text = text + ',';
+            append(text);
     }
     append("]");
-
 
     window.blob = new Blob([data.innerText], {type: 'application/octet-binary'});
     window.url = URL.createObjectURL(blob);
@@ -39,18 +38,18 @@ var download = function(format) {
     pom.click();
 
     window.close();
-  });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  window.data = document.getElementById('data');
+    window.data = document.getElementById('data');
 
-  document.getElementById('alldata').onclick = function() {
-    download('alldata');
-  };
+    document.getElementById('alldata').onclick = function() {
+        download('alldata');
+    };
 
-  document.getElementById('urlsonly').onclick = function() {
-    download('urlsonly');
-  };
+    document.getElementById('urlsonly').onclick = function() {
+        download('urlsonly');
+    };
 });
 
